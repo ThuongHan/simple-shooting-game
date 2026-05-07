f466768f051f8a0efbfba23abf2c11b62c64ce0f
ThuongHan <mateusz.susmarski@gmail.com>
2026-05-07 02:20:50 +0200

    m

---
+++ src/build/CMakeFiles/VirusGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VirusGame.dir/VirusGame.cpp.o"
+  "CMakeFiles/VirusGame.dir/VirusGame.cpp.o.d"
+  "VirusGame"
+  "VirusGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VirusGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
